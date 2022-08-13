a09a75ae31cc1464d7996b1ab9439ce8399bda10
Kronos <1963024305@qq.com>
2022-08-13 15:28:36 +0800

    添加测量距离

--- src/components/main/cesium.tsx
+++ src/components/main/cesium.tsx
@@ -158,12 +158,12 @@ const CesiumComponent: React.FC<{}> = () => {
             menu.appendChild(option);
           }
         },
-        reset: function () {},
+        reset: function () {}, 
       };
       //@ts-ignore
       Sandcastle.addDefaultToolbarButton("Satellites", function () {
         // 读取轨迹数据
-        let dronePromise = CM.CzmlDataSource.load("./data/starlink-2720.czml");
+        let dronePromise = CM.CzmlDataSource.load("./data/starlink-3.czml");
         // 加载实体
         let drone;
         dronePromise.then((dataSource: any) => {
@@ -194,7 +194,7 @@ const CesiumComponent: React.FC<{}> = () => {
             //   //配置模型轮廓的颜色
             //   silhouetteColor: CM.Color.WHITE,
             //   //配置轮廓的大小
-            //   silhouetteSize: 1,
+            //   silhouetteSize: 0,
             // };
             // //设置方向,根据实体的位置来配置方向
             // ele.orientation = new CM.VelocityOrientationProperty(ele.position);
@@ -255,8 +255,151 @@ const CesiumComponent: React.FC<{}> = () => {
           }
         }
       }, CM.ScreenSpaceEventType.RIGHT_CLICK);
+      // 绘制测量距离线条
+      drawDistanceLine(viewer);
     }
   }, [init]);
+
+  // 绘制线条测量距离
+const drawDistanceLine = (viewer: any) => {
+  viewer.cesiumWidget.screenSpaceEventHandler.removeInputAction(
+    CM.ScreenSpaceEventType.LEFT_DOUBLE_CLICK
+  );
+
+  let handler = new CM.ScreenSpaceEventHandler(
+    viewer.scene._imageryLayerCollection
+  );
+  var positions: any[] = [];
+  var poly: any = null;
+  // var tooltip = document.getElementById("toolTip");
+  var distance: string | null = '0';
+  var cartesian = null;
+  var floatingPoint;
+  // tooltip.style.display = "block";
+
+  handler.setInputAction(function (movement: { endPosition: any; }) {
+    // tooltip.style.left = movement.endPosition.x + 3 + "px";
+    // tooltip.style.top = movement.endPosition.y - 25 + "px";
+    // tooltip.innerHTML = '<p>单击开始，右击结束</p>';
+    // cartesian = viewer.scene.pickPosition(movement.endPosition);
+    let ray = viewer.camera.getPickRay(movement.endPosition);
+    cartesian = viewer.scene.globe.pick(ray, viewer.scene);
+    //cartesian = viewer.scene.camera.pickEllipsoid(movement.endPosition, viewer.scene.globe.ellipsoid);
+    if (positions.length >= 2) {
+      if (!CM.defined(poly)) {
+        //@ts-ignore
+        poly = new PolyLinePrimitive(positions);
+      } else {
+        positions.pop();
+        // cartesian.y += (1 + Math.random());
+        positions.push(cartesian);
+      }
+      distance = getSpaceDistance(positions);
+      // console.log("distance: " + distance);
+      // tooltip.innerHTML='<p>'+distance+'米</p>';
+    }
+  }, CM.ScreenSpaceEventType.MOUSE_MOVE);
+
+  handler.setInputAction(function (movement: { position: any; }) {
+    // tooltip.style.display = "none";
+    // cartesian = viewer.scene.camera.pickEllipsoid(movement.position, viewer.scene.globe.ellipsoid);
+    // cartesian = viewer.scene.pickPosition(movement.position);
+    let ray = viewer.camera.getPickRay(movement.position);
+    cartesian = viewer.scene.globe.pick(ray, viewer.scene);
+    if (positions.length == 0) {
+      positions.push(cartesian.clone());
+    }
+    positions.push(cartesian);
+    //在三维场景中添加Label
+    //   var cartographic = Cesium.Cartographic.fromCartesian(cartesian);
+    var textDisance = distance + '米';
+    // console.log(textDisance + ",lng:" + cartographic.longitude/Math.PI*180.0);
+    floatingPoint = viewer.entities.add({
+      name: '空间直线距离',
+      // position: Cesium.Cartesian3.fromDegrees(cartographic.longitude / Math.PI * 180, cartographic.latitude / Math.PI * 180,cartographic.height),
+      position: positions[positions.length - 1],
+      point: {
+        pixelSize: 5,
+        color: CM.Color.RED,
+        outlineColor: CM.Color.WHITE,
+        outlineWidth: 2,
+      },
+      label: {
+        text: textDisance,
+        font: '18px sans-serif',
+        fillColor: CM.Color.GOLD,
+        style: CM.LabelStyle.FILL_AND_OUTLINE,
+        outlineWidth: 2,
+        verticalOrigin: CM.VerticalOrigin.BOTTOM,
+        pixelOffset: new CM.Cartesian2(20, -20),
+      },
+    });
+  }, CM.ScreenSpaceEventType.LEFT_CLICK);
+
+  handler.setInputAction(function () {
+    handler.destroy(); //关闭事件句柄
+    positions.pop(); //最后一个点无效
+    // viewer.entities.remove(floatingPoint);
+    // tooltip.style.display = "none";
+  }, CM.ScreenSpaceEventType.RIGHT_CLICK);
+
+  var PolyLinePrimitive = (function () {
+    function _(this: any, positions: any) {
+      this.options = {
+        name: '直线',
+        polyline: {
+          show: true,
+          positions: [],
+          material: CM.Color.CHARTREUSE,
+          width: 10,
+          clampToGround: true,
+        },
+      };
+      this.positions = positions;
+      this._init();
+    }
+
+    _.prototype._init = function () {
+      var _self = this;
+      var _update = function () {
+        return _self.positions;
+      };
+      //实时更新polyline.positions
+      this.options.polyline.positions = new CM.CallbackProperty(
+        _update,
+        false
+      );
+      viewer.entities.add(this.options);
+    };
+
+    return _;
+  })();
+
+  //空间两点距离计算函数
+  function getSpaceDistance(positions: string | any[]) {
+    var distance = 0;
+    for (var i = 0; i < positions.length - 1; i++) {
+      var point1cartographic = CM.Cartographic.fromCartesian(
+        positions[i]
+      );
+      var point2cartographic = CM.Cartographic.fromCartesian(
+        positions[i + 1]
+      );
+      /**根据经纬度计算出距离**/
+      var geodesic = new CM.EllipsoidGeodesic();
+      geodesic.setEndPoints(point1cartographic, point2cartographic);
+      var s = geodesic.surfaceDistance;
+      //console.log(Math.sqrt(Math.pow(distance, 2) + Math.pow(endheight, 2)));
+      //返回两点之间的距离
+      s = Math.sqrt(
+        Math.pow(s, 2) +
+          Math.pow(point2cartographic.height - point1cartographic.height, 2)
+      );
+      distance = distance + s;
+    }
+    return distance.toFixed(2);
+  }
+}
 
   return (
     <>
